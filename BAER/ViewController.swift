d9a6457f1d16e719badf5778517334568401b24c
Shouki Souri <shoukisouri@shoukis-macbook-pro.local>
2017-07-05 13:09:04 -0700

    Initial Commit

---
+++ BAER/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BAER
+//
+//  Created by Shouki Souri on 7/5/17.
+//  Copyright © 2017 Shouki Souri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
